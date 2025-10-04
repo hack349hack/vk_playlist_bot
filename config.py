a80e882140612e906a64bf67fdbed0c6a4b1bb3d
hack349hack <115045581+hack349hack@users.noreply.github.com>
2025-10-05 00:38:19 +0300

    Update config.py

--- config.py
+++ config.py
@@ -3,3 +3,6 @@ import os
 BOT_TOKEN = os.getenv('BOT_TOKEN', 'YOUR_BOT_TOKEN')
 MIN_LISTENS = 200
 MAX_PLAYLISTS_TO_SHOW = 20
+
+# Для Web App
+WEBAPP_URL = os.getenv('WEBAPP_URL', 'https://your-webapp-domain.com')
